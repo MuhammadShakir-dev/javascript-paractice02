2d156d1ae3c55e7f8c45313a89f44c5986309753
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-21 12:14:45 +0500

    Update practice-01.js

--- practice-01.js
+++ practice-01.js
@@ -1,1 +1,34 @@
 
+ <script>
+       /*
+                 1. Write a JavaScript program to list the properties of a JavaScript object.
+
+
+
+
+          */
+                         var a = {
+                            Name : "Muhammad Shakir",
+                            Age : "21",
+                            Degree : "BS-SE",
+                            University : "Sindh Madressatul Islam University",
+                         };
+
+                          for(i in a){
+                                 document.write(i + " : " + a[i] + "<br>");
+                          }
+                        
+                          document.write("<br>")
+
+                          var b = {
+                              Name : "Anum Mustafa",
+                              Age : 22 ,
+                              Degree : "BS-SE",
+                              University : "Sindh Madressatul Islam University"
+                          };
+
+
+                          for(j in b){
+                            document.write(j + " : " + a[j] + "<br>" );
+                          }
+    </script>
