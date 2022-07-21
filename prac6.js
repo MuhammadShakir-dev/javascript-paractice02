7aea0111e9e78b3fdc1a39b50242a50ec66c532f
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-21 13:42:23 +0500

    Create prac6.js

---
+++ prac6.js
@@ -0,0 +1,26 @@
+  <script>
+        /*
+                array map() method
+
+                array map method make a new array or object according to your condition
+
+        */
+                       var ary = [1,2,3,4,5,6,7,8,9,10];
+                       var b = ary.map(table);
+                       function table(x){
+                        return x * 2 
+                       }
+
+                       document.write(b + "<br>");
+
+
+                       var table3 = [1,2,3,4,5,6,7,8,9,10];
+                       var c = table3.map(tab);
+                       function tab (a){
+                        return a * 3;
+                       };
+
+                       document.write(c + "<br>");
+                       
+                       
+    </script>
