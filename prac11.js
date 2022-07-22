ea5f0860d1a981797078faab78219eee23044411
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-22 15:51:41 +0500

    Update prac11.js

--- prac11.js
+++ prac11.js
@@ -1,1 +1,17 @@
 
+ <script>
+        /*
+                 indexOf and lastIndexOf() in javascript.
+
+
+
+        */
+                      var a = ["Anum","Shakir","Shiza","Shakir"];
+                      var b = a.indexOf("Shiza",0);
+                      document.write(b + "<br><br>");
+
+
+                      var c = ["Anum","Shiza","Shakir","Shakir"];
+                      var d = a.lastIndexOf("Shakir",2);
+                      document.write(d);
+    </script>
