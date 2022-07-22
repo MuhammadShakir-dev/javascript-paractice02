622ac3e38c9a525d5c1178fe3e58b2c1c6750d86
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-22 16:02:40 +0500

    Create prac13.js

---
+++ prac13.js
@@ -0,0 +1,13 @@
+  <script>
+        /*
+                    array includes() in javascript
+
+                    ary includes give you the anwer of your array test in boolean,
+
+
+
+        */
+                        var a = [31,33,31,31,22,56,77,89,35,32,31];
+                        var b = a.includes(31,1);
+                        document.write(b);
+    </script>
