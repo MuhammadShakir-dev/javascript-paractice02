55109bb31f7b0ea4da2b77ac0da5b9acc6ae5b33
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-22 12:33:24 +0500

    Update prac9.js

--- prac9.js
+++ prac9.js
@@ -1,1 +1,26 @@
 
+    <script>
+      /*
+                    multidimensional array in javascript
+
+
+
+        */
+      var ary = [
+        ["Muhammad Shakir", "BS-SE", "21", "KARACHI","Undergraduate"],
+        ["Anum Mustafa", "BS-SE", "22", "KARACHI","Undergraduate"],
+      ];
+
+      document.write("<table border = '1px' cellspacing = '0px'>");
+      for (var i = 0; i < ary.length; i++) {
+        document.write("<br");
+        document.write("<tr>");
+        for (var j = 0; j < 5; j++) {
+          document.write("<td>" + ary[i][j] + "</td> ");
+        }
+        document.write("</tr>");
+      }
+      document.write("</table>");
+
+
+    </script>
