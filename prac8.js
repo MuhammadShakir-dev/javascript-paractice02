5e6af494c8f9f1113b9708c80ba9559c07ce19de
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-22 12:18:34 +0500

    Update prac8.js

--- prac8.js
+++ prac8.js
@@ -1,1 +1,19 @@
 
+  <script>
+        /*
+                      const variable in javascript
+
+        */
+
+                        const a = [10,20,30,40,77];
+                        document.write("<h1>Actual array</h1>")
+                        document.write(a + "<br><br>");
+
+                        a[3] = 49;
+                        a[2] = 31;
+                        document.write("<h1>Updated array</h1>")
+                        document.write(a + "<br>")
+
+
+
+    </script>
