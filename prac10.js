72b1877e90c0ccef9aae8f3764d01b609acd7539
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-22 15:41:30 +0500

    Update prac10.js

--- prac10.js
+++ prac10.js
@@ -1,1 +1,18 @@
 
+   <script>
+        /*
+                  shift and unshift() in javascript
+
+               shift delete the first element from your array.
+
+               unshift add a new element at the begining of your array.
+        
+        */
+
+              var ary = ["Maira","Anum","Shiza","Shakir"];
+              ary.shift(0);
+              document.write(ary + "<br><br>");
+
+              ary.unshift("SAS = ");
+              document.write(ary);
+    </script>
