00ac316df53004a920b50d994675eda4baac18b2
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-23 12:40:53 +0500

    Create prac16.js

---
+++ prac16.js
@@ -0,0 +1,20 @@
+  <script>
+        /*
+                  isArray in javascript 
+
+                  is used to find that the variable is either array or not.
+
+
+
+        */
+                   var a = [12,34,56,78,98,3,45];
+                   var b = Array.isArray(a);
+                   document.write("The given variable is array ? " + b + "<br><br>");
+
+                   var b = {
+                    name : "Muhammad Shakir",
+                    age : 21
+                   };
+                   var c = Array.isArray(b);
+                   document.write("The given variable is array ? " + c);
+    </script>
