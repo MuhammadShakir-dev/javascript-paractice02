a321ee6b34e14d39580a8978d5d3e5e604255339
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-23 12:34:59 +0500

    Update prac15.js

--- prac15.js
+++ prac15.js
@@ -1,1 +1,6 @@
+    var ary = ["GITHUB","HASHNODE","MEDIUM","DEVCOMMUNITY"];
+                    var b = ary.forEach(function(value, index){
+                    
+                    document.write(index + " : " + value + "<br>");
+                     });
 
